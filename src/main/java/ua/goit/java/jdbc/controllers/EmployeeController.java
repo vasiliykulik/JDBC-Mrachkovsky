c118cdb42754f2b427e3e60c66eb6a79110b371a
Стрела <detroit.vv@gmail.com>
2016-12-07 17:43:05 +0200

    JDBC part 3 SPRING_Transactions

---
+++ src/main/java/ua/goit/java/jdbc/controllers/EmployeeController.java
@@ -0,0 +1,22 @@
+package ua.goit.java.jdbc.controllers;
+
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+import ua.goit.java.jdbc.model.Employee;
+
+import java.util.List;
+
+/**
+ * Created by Стрела on 06.12.2016.
+ */
+public class EmployeeController {
+// 2. с помощью которого будем открывать транзакцию и закрывать транзакцию
+    private PlatformTransactionManager txManager;
+    // 1. будет поддержка транзакций
+    public List<Employee> getAllEmployees(){
+
+        TransactionStatus status = txManager.getTransaction(new DefaultTransactionDefinition())
+
+    }
+}
